f7527653aff8b35d6e577f72867ea345d3c03937
Edwingudfriend <edwingudfriend@gmail.com>
2023-03-17 18:29:21 +0300

    Initial commit

---
+++ src/main/java/com/example/bkonnect202317031645/Bkonnect202317031645Application.java
@@ -0,0 +1,13 @@
+package com.example.bkonnect202317031645;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bkonnect202317031645Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Bkonnect202317031645Application.class, args);
+    }
+
+}
