143680e489842593147f24180c1b44861a6535c1
Edwingudfriend <edwingudfriend@gmail.com>
2023-03-17 18:34:33 +0300

    I have added comment

---
+++ src/main/java/com/example/bkonnect202317031645/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.example.bkonnect202317031645.repository;
+
+import com.example.bkonnect202317031645.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+//AADDED COMMENT
+public interface UserRepository extends JpaRepository <User,Long > {
+
+}
